03aab3f71defd2e7fae5dcf27e154b6e40ee1c79
wxzy2 <165672074+wxzy2@users.noreply.github.com>
2025-12-24 15:21:23 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,22 +1,17 @@
 #!/bin/bash
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2.sh
-# Description: OpenWrt DIY script part 2 (After Update feeds)
-#
-# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
 
-# Modify default IP
-# sed -i 's/192.168.1.1/10.1.1.1/g' package/base-files/files/bin/config_generate
-# rm -rf feeds/smpackage/{sing-box}
-# rm -rf feeds/package/{mwan3,ddns-go,passwall}
-# rm -rf feeds/smpackage/{base-files,dnsmasq,dnsproxy,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd*,miniupnpd-iptables,wireless-regdb}
-# Modify default theme
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+# 1. 修改默认 IP 为 10.1.1.1
+sed -i 's/192.168.1.1/10.1.1.1/g' package/base-files/files/bin/config_generate
 
-# Modify hostname
-#sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+# 2. 替换 Athena (雅典娜) 的 LED 驱动
+rm -rf package/emortal/luci-app-athena-led
+git clone --depth=1 https://github.com/NONGFAH/luci-app-athena-led package/luci-app-athena-led
+chmod +x package/luci-app-athena-led/root/etc/init.d/athena_led
+chmod +x package/luci-app-athena-led/root/usr/sbin/athena-led
+
+# 3. 彻底物理删除测速插件 (防止被 small-package 自动装回去)
+rm -rf feeds/small8/luci-app-netspeedtest
+rm -rf feeds/small8/speedtest-cli
+
+# 4. 彻底物理删除在线升级插件
+rm -rf feeds/luci/applications/luci-app-attendedsysupgrade
