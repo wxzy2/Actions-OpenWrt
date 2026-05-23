ece6c50d67d79b44432a71bc9a3b3705a45e29de
wxzy2 <165672074+wxzy2@users.noreply.github.com>
2026-05-23 14:26:19 +0800

    Create diy.sh

---
+++ diy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# 1. 修改默认 LAN IP 为 10.1.1.1
+sed -i 's/192.168.1.1/10.1.1.1/g' package/base-files/files/bin/config_generate
+
+# 2. 注入极光主题 (luci-theme-aurora)
+echo "src-git aurora_theme https://github.com/eamonxg/luci-theme-aurora.git;main" >> feeds.conf.default
+
+# 3. 注入 ddns-go 核心源
+echo "src-git ddnsgo https://github.com/sirpdboy/luci-app-ddns-go.git;main" >> feeds.conf.default
+
+# 4. 注入 OpenClash 核心源
+echo "src-git openclash https://github.com/vernesong/OpenClash.git;master" >> feeds.conf.default
+
+# 5. 注入常用精选插件集合源 (包含 passwall, aria2, smartdns 等)
+echo "src-git modern_apps https://github.com/fw876/helloworld.git;master" >> feeds.conf.default
+echo "src-git small8 https://github.com/kenzok8/small-package.git;main" >> feeds.conf.default
