49a29211b7bf89f86364c648169dc112ed29e318
wxzy2 <165672074+wxzy2@users.noreply.github.com>
2026-05-25 15:42:24 +0800

    ai

--- diy-part1.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# 添加 kenzok8/small-package 软件源
-echo "src-git small8 https://github.com/kenzok8/small-package" >> openwrt/feeds.conf.default
-git clone --depth 1 https://github.com/eamonxg/luci-theme-aurora package/luci-theme-aurora
